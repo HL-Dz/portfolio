a29753d3ae5a7dc5e597c3f57bd7d5101a493eb1
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2020-12-28 20:42:47 +0300

    feat: add greet layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/main.scss';
+
+
+const btn = document.querySelector('.show');
+btn.onclick = () => {
+  let technologies = document.querySelector('.technologies');
+  technologies.classList.add('technologies_close');
+  setTimeout(() => {
+    document.querySelector('.info').classList.add('info_inactive');
+  }, 1000);
+  setTimeout(() => {
+    document.querySelector('.block-top').classList.add('block-top_active');
+    document.querySelector('.block-bottom').classList.add('block-bottom_active');
+  }, 2000);
+}
