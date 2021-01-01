1ae29314d7ccd18439833f5ad24cccdd6fc8d3cf
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2021-01-01 21:34:37 +0300

    feat: implement basic router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import './sass/main.scss';
 import Greeting from './js-modules/greeting.js';
-import technologies from './js-modules/data.js'
+import technologies from './js-modules/data.js';
+import locationResolver from './js-modules/locationResolver.js';
+
+const app = document.querySelector('#app');
+const navigation = document.querySelector('.navigation-block');
+const navLinks = document.querySelectorAll('.nav__link');
+
+window.addEventListener('load', () => {
+  let location = window.location.hash;
+  if(location) {
+    locationResolver(app, location);
+  }
+})
+
 
 const greeting = new Greeting({
   data: technologies,
   fullname: 'Dzmitry Hlushak',
   profession: 'Frontend developer'
 });
+
+navigation.addEventListener('click', (e) => {
+  let target = e.target.closest('[data-type="route-btn"]')
+  if(target) {
+    let href = target.dataset.href;
+    navLinks.forEach(link => {
+      link.classList.remove('nav__link_active')
+    })
+    target.classList.add('nav__link_active');
+    locationResolver(app, href);
+  }
+})
---
+++ src/js-modules/locationResolver.js
@@ -0,0 +1,25 @@
+const locationResolver = (selector, location) => {
+  switch(location) {
+    case '#/about/':
+      selector.innerHTML = `
+        <h1 class="about">About</h1>
+        <p class="about__text">This is a page about me!</p>
+      `
+      break;
+    case '#/portfolio/':
+      selector.innerHTML = `
+        <h1 class="about">Portfolio</h1>
+        <p class="about__text">This is a portfolio page</p>
+      `
+      break;
+    case '#/contact/':
+      selector.innerHTML = `
+        <h1 class="about">Contact</h1>
+        <p class="about__text">This is a contact page</p>
+      `
+      break;
+  }
+};
+
+
+export default locationResolver;
