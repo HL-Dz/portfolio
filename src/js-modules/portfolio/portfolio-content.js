de35ec587a8e38deb3e8979fe2d7c8891d2d3cba
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2021-01-11 23:02:06 +0300

    feat: add data for portfolio section

--- src/js-modules/portfolio/portfolio-content.js
+++ src/js-modules/portfolio/portfolio-content.js
@@ -1,63 +1,46 @@
+import { works } from './portfolio-data.js';
+
+const getWork = (el) => {
+  return `
+  <div class="box">
+    <div class="box__inner">
+      <div class="box__front">
+        <img class="box__img" src="${el.imgPath}" alt="${el.name}">
+        <div class="box__description">
+          <div class="box__text">${el.title}</div>
+        </div>
+      </div>
+      <div class="box__back">
+        <div class="box__info">
+          Смотреть исходники на Github + Production.
+        </div>
+        <div class="source">
+          <a class="source__link source__link_code" href="${el.codeLink}" target="_blank" title="Исходники">
+            <i class="fas fa-file-code source-code"></i>
+          </a>
+          <a class="source__link source__link_watch" href="${el.prodLink}" target="_blank" title="Production">
+            <i class="fas fa-eye source-watch"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+}
+
 const renderPortfolio = () => {
+  const allWorks = works.map(elem => {
+    return getWork(elem);
+  })
+
+
   return `
     <div class="portfolio">
       <div class="portfolio__img"></div>
       <div class="container-block">
         <h1 class="portfolio__title">Портфолио</h1>
         <div class="portfolio-container">
-
-          <div class="box">
-            <div class="box__inner">
-              <div class="box__front">
-                <img class="box__img" src="./img/keyboard.png" alt="Keyboard">
-                <div class="box__description">
-                  <div class="box__text">
-                    Виртуальная клавиатура + TodoList
-                  </div>
-                </div>
-              </div>
-              <div class="box__back">
-                <div class="box__info">
-                  Смотреть исходники на Github + Production.
-                </div>
-                <div class="source">
-                  <a class="source__link source__link_code" href="https://github.com/HL-Dz/virtual-keyboard-app" target="_blank" title="Исходники">
-                    <i class="fas fa-file-code source-code"></i>
-                  </a>
-                  <a class="source__link source__link_watch" href="https://virtual-keyboard-app1.netlify.app/" target="_blank" title="Production">
-                    <i class="fas fa-eye source-watch"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="box">
-            <div class="box__inner">
-              <div class="box__front">
-                <img class="box__img" src="./img/singolo.png" alt="Singolo">
-                <div class="box__description">
-                  <div class="box__text">
-                    Адаптивный лендинг
-                  </div>
-                </div>
-              </div>
-              <div class="box__back">
-                <div class="box__info">
-                  Смотреть исходники на Github + Production.
-                </div>
-                <div class="source">
-                  <a class="source__link source__link_code" href="#" target="_blank" title="Исходники">
-                    <i class="fas fa-file-code source-code"></i>
-                  </a>
-                  <a class="source__link source__link_watch" href="#" target="_blank" title="Production">
-                    <i class="fas fa-eye source-watch"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          
+          ${allWorks.join('')}
         </div>
       </div>
     </div>
---
+++ src/js-modules/portfolio/portfolio-data.js
@@ -0,0 +1,42 @@
+export let works = [
+  {
+    id: 1,
+    title: `Виртуальная клавиатура + TodoList`,
+    name: 'Keyboard',
+    imgPath: './img/keyboard.png',
+    codeLink: 'https://github.com/HL-Dz/virtual-keyboard-app',
+    prodLink: 'https://virtual-keyboard-app1.netlify.app/'
+  },
+  {
+    id: 2,
+    title: `Адаптивный лендинг`,
+    name: 'Singolo',
+    imgPath: './img/singolo.png',
+    codeLink: 'https://github.com/HL-Dz/singolo',
+    prodLink: 'https://singolo-app.netlify.app/'
+  },
+  {
+    id: 3,
+    title: `Статический лендинг`,
+    name: 'Burgers',
+    imgPath: './img/burgers.png',
+    codeLink: 'https://github.com/HL-Dz/burgers',
+    prodLink: 'https://new-burgers-app.netlify.app/'
+  },
+  {
+    id: 4,
+    title: `Базовый шаблон`,
+    name: 'Plane',
+    imgPath: './img/plane.png',
+    codeLink: 'https://github.com/HL-Dz/HL-Dz.github.io/tree/master/test',
+    prodLink: 'https://plane-app.netlify.app/'
+  },
+  {
+    id: 5,
+    title: `Базовый шаблон`,
+    name: 'Social',
+    imgPath: './img/Social.png',
+    codeLink: 'https://github.com/HL-Dz/HL-Dz.github.io/tree/master/work1',
+    prodLink: 'https://social-ui-1.netlify.app/'
+  },
+]
