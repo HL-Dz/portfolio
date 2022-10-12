2c3175094136659d74a802d6fca41f15ac51f1fe
Hlushak Dzmitry <hlushak.dzmitry@gmail.com>
2022-10-12 16:19:52 +0300

    change data

--- src/js-modules/portfolio/portfolio-data.js
+++ src/js-modules/portfolio/portfolio-data.js
@@ -1,83 +1,85 @@
 export let works = [
   {
     id: 1,
+    title: `TFL - TOP FOOTBALL LEAGUES`,
+    name: "TFL",
+    imgPath: "./img/tfl.png",
+    codeLink: "https://github.com/HL-Dz/virtual-keyboard-app",
+    prodLink: "https://tfl-app.netlify.app/",
+    description: `
+    Приложение предназначено для всех пользователей, но больше для любителей футбола.
+    В приложении вы можете найти информацию о футбольных лигах, командах, игроках.
+    Вы можете читать статьи, комментировать их.
+    Статьи добавляются администраторами приложения.
+    Приложение имеет ограничение на количество запросов к данным.
+    Если лимит запроса превышен, в модальном окне будет отображено сообщение.
+    `,
+    techs: ["./img/js.png", "./img/sass.png", "./img/webpack.png"],
+  },
+  {
+    id: 2,
     title: `Виртуальная клавиатура + TodoList`,
-    name: 'Keyboard',
-    imgPath: './img/keyboard.png',
-    codeLink: 'https://github.com/HL-Dz/virtual-keyboard-app',
-    prodLink: 'https://virtual-keyboard-app1.netlify.app/',
+    name: "Keyboard",
+    imgPath: "./img/keyboard.png",
+    codeLink: "https://github.com/HL-Dz/virtual-keyboard-app",
+    prodLink: "https://virtual-keyboard-app1.netlify.app/",
     description: `
     Виртуальная клавиатура поддерживает несколько языков.
     Легко можно добавить нужный язык через данные JS.
     Клавиатура совмещена с ToDoList. В нем отображается список задач.
     Все задачи хранятся в localStorage.
     `,
-    techs: [
-      './img/js.png',
-      './img/sass.png',
-      './img/webpack.png'
-    ]
-  },
-  {
-    id: 2,
-    title: `Адаптивный лендинг`,
-    name: 'Singolo',
-    imgPath: './img/singolo.png',
-    codeLink: 'https://github.com/HL-Dz/singolo',
-    prodLink: 'https://singolo-app.netlify.app/',
-    description: `
-    Универсальный адаптивный ленгдинг. Имеется 5 разделов с определенной тематикой.
-    `,
-    techs: [
-      './img/js.png',
-      './img/css.png',
-      './img/html.png',
-    ]
+    techs: ["./img/js.png", "./img/sass.png", "./img/webpack.png"],
   },
   {
     id: 3,
+    title: `Адаптивный лендинг`,
+    name: "Singolo",
+    imgPath: "./img/singolo.png",
+    codeLink: "https://github.com/HL-Dz/singolo",
+    prodLink: "https://singolo-app.netlify.app/",
+    description: `
+    Универсальный адаптивный ленгдинг. Имеется 5 разделов с определенной тематикой.
+    `,
+    techs: ["./img/js.png", "./img/css.png", "./img/html.png"],
+  },
+  {
+    id: 4,
     title: `Статический лендинг`,
-    name: 'Burgers',
-    imgPath: './img/burgers.png',
-    codeLink: 'https://github.com/HL-Dz/burgers',
-    prodLink: 'https://new-burgers-app.netlify.app/',
+    name: "Burgers",
+    imgPath: "./img/burgers.png",
+    codeLink: "https://github.com/HL-Dz/burgers",
+    prodLink: "https://new-burgers-app.netlify.app/",
     description: `
     Сайт для ресторана быстрого питания. 
     Фиксированные размеры блоков. На мобильных устройствах отображаются
     в масштабе 1.0.
     `,
     techs: [
-      './img/jquery.png',
-      './img/sass.png',
-      './img/html.png',
-      './img/webpack.png'
-    ]
-  },
-  {
-    id: 4,
-    title: `Адаптивная верстка`,
-    name: 'Plane',
-    imgPath: './img/plane.png',
-    codeLink: 'https://github.com/HL-Dz/HL-Dz.github.io/tree/master/test',
-    prodLink: 'https://plane-app.netlify.app/',
-    description: `Адаптивная одностраничная верстка.`,
-    techs: [
-      './img/js.png',
-      './img/sass.png',
-      './img/html.png',
-    ]
+      "./img/jquery.png",
+      "./img/sass.png",
+      "./img/html.png",
+      "./img/webpack.png",
+    ],
   },
   {
     id: 5,
     title: `Адаптивная верстка`,
-    name: 'Social',
-    imgPath: './img/social.png',
-    codeLink: 'https://github.com/HL-Dz/HL-Dz.github.io/tree/master/work1',
-    prodLink: 'https://social-ui-1.netlify.app/',
+    name: "Plane",
+    imgPath: "./img/plane.png",
+    codeLink: "https://github.com/HL-Dz/HL-Dz.github.io/tree/master/test",
+    prodLink: "https://plane-app.netlify.app/",
     description: `Адаптивная одностраничная верстка.`,
-    techs: [
-      './img/sass.png',
-      './img/html.png',
-    ]
+    techs: ["./img/js.png", "./img/sass.png", "./img/html.png"],
   },
-]
+  {
+    id: 6,
+    title: `Адаптивная верстка`,
+    name: "Social",
+    imgPath: "./img/social.png",
+    codeLink: "https://github.com/HL-Dz/HL-Dz.github.io/tree/master/work1",
+    prodLink: "https://social-ui-1.netlify.app/",
+    description: `Адаптивная одностраничная верстка.`,
+    techs: ["./img/sass.png", "./img/html.png"],
+  },
+];
